2ec8aa3fca27ebd437932cb8ed2c303bff4f0121
Hafshovra <44991395+Hafshovra@users.noreply.github.com>
2019-01-22 08:09:04 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!-- Start Footer -->
+<head>
+   <link rel="stylesheet" type="text/css" href="css/style.css" />
+   
+</head>
+
+
+  	<div class="row">
+  	<div class="main_footer">
+         <div class="row">
+            <div class="col-md-9 col-sm-8 col-xs-12">All &copy; rights <?php echo date(Y); ?>  <a href="http://www.freetimelearning.com" style="text-decoration:none; color:#FF0;" target="_blank"> Free Time Learning </a>. </div>
+            <div class="col-md-3 col-sm-4 col-xs-12" align="center">Designed by <a href="http://www.freetimelearning.com" style="text-decoration:none; color:#FF0;" target="_blank"> F T L </a>.</div>
+         </div>
+    </div>
+  </div>  
+<!-- End Footer -->
